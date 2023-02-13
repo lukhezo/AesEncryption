74f9dd9b2660cfa82f67db4da14fd5bdc98a1a29
Ira Lukhezo <ira.lukhezo@brokernetwork.co.uk>
2023-02-13 17:43:26 +0000

    Add project files.

---
+++ AesEncryption/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AesEncryption;
+string password = @"X@X4v3kGY;56#\\2Ez6?R8<#S";
+var message = "Hello, World!";
+var encypted = StringCipher.Encrypt(message, password);
+var decrypted = StringCipher.Decrypt(encypted, password);
+
+Console.WriteLine($"Encrypted: {encypted}");
+Console.WriteLine($"Decrypted: {decrypted}");
+Console.ReadKey();
